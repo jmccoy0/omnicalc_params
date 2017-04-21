5cd6eb92bf0b854cb5bb120025a7d3925916da75
Jessica McCoy <jmccoy0@users.noreply.github.com>
2017-04-21 14:03:48 -0500

    In class work

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,27 @@
+class CalculationsController < ApplicationController
+# ApplicationController is a master class - CalculationsController is inheriting those characteristics
+
+  def square_root
+
+    render("calculations/square_root.html.erb")
+  end
+
+  def square
+    @user_number = params[:user_number].to_f
+    # user_number was defined in html code in the form
+    @square = @user_number**2
+    render("calculations/square.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def flex_square_5
+    # Parameters: {"num"=>"5"}
+    @user_number = params["num"].to_f
+    @square = @user_number**2
+    render("calculations/flex_square_5.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
+  get("/square/new", {:controller => "calculations", :action => "square_form"})
+  get("/square/results", {:controller => "calculations", :action => "square"})
+  get("/square_root/new", {:controller => "calculations", :action => "square"})
 end
